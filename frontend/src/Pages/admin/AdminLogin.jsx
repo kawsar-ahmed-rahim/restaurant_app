cbe57f0f720de78b3f36b1c9316bfbded546ffb2
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-04-06 23:28:18 +0600

    till now

--- frontend/src/Pages/admin/AdminLogin.jsx
+++ frontend/src/Pages/admin/AdminLogin.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { LockIcon, MailIcon } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { AppContext } from "../../context/AppContext";
 const AdminLogin = () => {
-  return (
-    <div>AdminLogin</div>
-  )
-}
+  const { navigate, axios, loading, setLoading, setAdmin } =
+    useContext(AppContext);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default AdminLogin
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/auth/admin/login", formData);
+      if (data.success) {
+        setAdmin(data.user);
+        toast.success(data.message);
+        navigate("/admin");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message || "Something went wrong!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="py-12 flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="py-12 sm:w-87.5 w-full text-center bg-gray-900 border border-gray-800 rounded-2xl px-8"
+      >
+        <h1 className="text-white text-3xl mt-10 font-medium">Login</h1>
+
+        <p className="text-gray-400 text-sm mt-2">Please Login to continue</p>
+
+        <div className="flex items-center w-full mt-4 bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
+          {/* mail icon */}
+          <MailIcon className="text-white" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email id"
+            className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none "
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className=" flex items-center mt-4 w-full bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
+          {/* lock icon */}
+          <LockIcon className="text-white" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="mt-6 w-full h-11 rounded-full text-white bg-orange-600 hover:bg-indigo-500 transition-opacity cursor-pointer"
+        >
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminLogin;
