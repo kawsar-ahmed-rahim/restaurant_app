899b8d89d886039e73d0707a4e60e9d90fe76709
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-04-24 12:53:08 +0600

    till now

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Categories from './admin/Categories';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Categories />
     </div>
   )
 }
