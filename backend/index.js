e02258263649be22cea64a6b151e6e5ddd873361
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-05-20 18:35:10 +0600

    till now

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ connectCloudinary();
 // CORS configuration
 const corsOptions = {
   origin:
-    process.env.FRONTEND_URL || "https://restaurantappfrontend-psi.vercel.app/",
+    process.env.FRONTEND_URL || "https://restaurantappfrontend-psi.vercel.app",
   credentials: true,
 };
 
